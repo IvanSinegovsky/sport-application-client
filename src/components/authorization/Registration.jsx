8e525669980762ecd020ccbe32be7994da241741
IvanSinegovsky <isinegovsky@gmail.com>
2021-04-21 12:32:22 +0300

    added registration fields

--- src/components/authorization/Registration.jsx
+++ src/components/authorization/Registration.jsx
@@ -6,11 +6,16 @@ import {registration} from "../../actions/user";
 const Registration = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
 
+    // TODO REPEAT PASSWORD FIELD AND MATCH IT
     return (
         <div className='authorization'>
             <div className="authorization__header">Регистрация</div>
             <Input value={email} setValue={setEmail} type="text" placeholder="Введите email..."/>
+            <Input value={firstname} setValue={setFirstname} type="text" placeholder="Введите имя..."/>
+            <Input value={lastname} setValue={setLastname} type="text" placeholder="Введите фамилию..."/>
             <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
             <button className="authorization__btn" onClick={() => registration(email, password)}>Зарегистрироваться</button>
         </div>
